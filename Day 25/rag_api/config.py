ab0da6b53fee67d8879e33a9abf7db9bea7185b9
Tanzeela Fatima <tanzeelafatima987@gmail.com>
2026-06-23 16:18:54 +0500

    added chunks data in config.py

--- Day 25/rag_api/config.py
+++ Day 25/rag_api/config.py
@@ -5,3 +5,15 @@ load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_MODEL = os.getenv("GROQ_MODEL")
+
+EMBEDDING_MODEL = os.getenv(
+    "EMBEDDING_MODEL",
+    "sentence-transformers/all-MiniLM-L6-v2"
+)
+
+CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", 1000))
+CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", 200))
+TOP_K = int(os.getenv("TOP_K", 5))
+
+FAISS_PATH = os.getenv("FAISS_PATH", "data/faiss_index")
+UPLOAD_PATH = os.getenv("UPLOAD_PATH", "data/uploads")
