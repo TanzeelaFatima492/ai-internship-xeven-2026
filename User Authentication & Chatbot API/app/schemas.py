8aa9a5d76e6cfc64a4e5cb22ac4ec12703383b1e
Tanzeela Fatima <tanzeelafatima987@gmail.com>
2026-06-30 15:04:52 +0500

    Create User Model & Registration API

--- User Authentication & Chatbot API/app/schemas.py
+++ User Authentication & Chatbot API/app/schemas.py
@@ -1,29 +1,16 @@
-from datetime import datetime
-
 from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
-    full_name: str
-    email: EmailStr
-    password: str
-
-
-class UserLogin(BaseModel):
+    username: str
     email: EmailStr
     password: str
 
 
 class UserResponse(BaseModel):
     id: int
-    full_name: str
+    username: str
     email: EmailStr
-    created_at: datetime
 
     class Config:
         from_attributes = True
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
