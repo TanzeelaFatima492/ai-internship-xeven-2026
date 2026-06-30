d9662640c5a6f9477ea5105687d3843a75b7d846
Tanzeela Fatima <tanzeelafatima987@gmail.com>
2026-06-30 11:59:06 +0500

    updating schemas file

--- User Authentication & Chatbot API/app/schemas.py
+++ User Authentication & Chatbot API/app/schemas.py
@@ -1,9 +1,15 @@
-from pydantic import BaseModel, EmailStr
 from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
     full_name: str
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
@@ -16,3 +22,8 @@ class UserResponse(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
