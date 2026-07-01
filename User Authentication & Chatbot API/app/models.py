829ca2ddc3cdee579c4298d14e6066fca56265c2
Tanzeela Fatima <tanzeelafatima987@gmail.com>
2026-07-01 11:17:37 +0500

    create a connverational model

--- User Authentication & Chatbot API/app/models.py
+++ User Authentication & Chatbot API/app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -10,3 +11,17 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+
+    conversations = relationship("Conversation", back_populates="user")
+
+
+class Conversation(Base):
+    __tablename__ = "conversations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    message = Column(String)
+    response = Column(String)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="conversations")
