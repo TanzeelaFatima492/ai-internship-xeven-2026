a77277a99f5b3b37ec184528bd46dae0ec8b6390
Tanzeela Fatima <tanzeelafatima987@gmail.com>
2026-07-06 14:47:14 +0500

    Upload API

--- Day 27/Full Stack RAG/app/main.py
+++ Day 27/Full Stack RAG/app/main.py
@@ -1,16 +1,31 @@
 from fastapi import FastAPI
+
 from app.database.database import Base
+from app.database.database import engine
+
 import app.models
 
+from app.routers import document_router
+
+
 Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="Restaurant RAG API"
 )
 
 
+app.include_router(
+    document_router.router
+)
+
+
 @app.get("/")
 def home():
+
     return {
+
         "message": "API Running Successfully"
+
     }
---
+++ Day 27/Full Stack RAG/app/routers/document_router.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import File
+from fastapi import HTTPException
+from fastapi import UploadFile
+
+from sqlalchemy.orm import Session
+
+from app.dependencies import get_db
+from app.models.document import Document
+from app.schemas.document_schema import DocumentResponse
+
+
+router = APIRouter(
+    prefix="/documents",
+    tags=["Documents"]
+)
+
+
+UPLOAD_FOLDER = Path("data/uploads")
+
+UPLOAD_FOLDER.mkdir(
+    parents=True,
+    exist_ok=True
+)
+
+
+@router.post(
+    "/upload",
+    response_model=DocumentResponse
+)
+async def upload_document(
+
+    file: UploadFile = File(...),
+
+    db: Session = Depends(get_db)
+
+):
+
+    if not file.filename.endswith(".pdf"):
+
+        raise HTTPException(
+
+            status_code=400,
+
+            detail="Only PDF files are allowed."
+
+        )
+
+    file_path = UPLOAD_FOLDER / file.filename
+
+    with open(
+
+        file_path,
+
+        "wb"
+
+    ) as buffer:
+
+        buffer.write(
+
+            await file.read()
+
+        )
+
+    document = Document(
+
+        filename=file.filename,
+
+        file_type="pdf"
+
+    )
+
+    db.add(document)
+
+    db.commit()
+
+    db.refresh(document)
+
+    return document
