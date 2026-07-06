2cfc7d83c47c542de4d873ad8de8d2c0a47f1b00
Tanzeela Fatima <tanzeelafatima987@gmail.com>
2026-07-06 13:05:47 +0500

    Connect upload

---
+++ Day 27/Full Stack RAG/app/rag/embedding_service.py
@@ -0,0 +1,18 @@
+from sentence_transformers import SentenceTransformer
+
+
+class EmbeddingService:
+
+    def __init__(self):
+
+        self.model = SentenceTransformer(
+            "all-MiniLM-L6-v2"
+        )
+
+    def embed(self, text: str):
+
+        return self.model.encode(text)
+
+    def embed_many(self, texts: list[str]):
+
+        return self.model.encode(texts)
